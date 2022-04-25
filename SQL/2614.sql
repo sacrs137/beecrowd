d93f90239dc3eeef26a4b80c019c249bdd353871
S <77491046+sviegasdc@users.noreply.github.com>
2022-04-25 20:46:30 -0300

    Create 2614.sql

---
+++ SQL/2614.sql
@@ -0,0 +1,4 @@
+SELECT cus.name, r.rentals_date FROM rentals r
+INNER JOIN customers cus ON cus.id = r.id_customers
+WHERE EXTRACT(month FROM r.rentals_date) = 9 
+AND EXTRACT(year FROM r.rentals_date) = 2016
