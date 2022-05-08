a8450ba7d11b955fade60a460abaaf9687a0d956
S <77491046+sviegasdc@users.noreply.github.com>
2022-05-08 11:55:06 -0300

    Update 2738.sql

--- SQL/2738.sql
+++ SQL/2738.sql
@@ -1,3 +1,3 @@
-SELECT c.name, cast((((s.math * 2) + (s.specific * 3) + (s.project_plan * 5)) / 10) AS NUMERIC(15, 2)) AS avg
+SELECT c.name, CAST((((s.math * 2) + (s.specific * 3) + (s.project_plan * 5)) / 10) AS NUMERIC(15, 2)) AS avg
 FROM candidate c INNER JOIN score s ON c.id = s.candidate_id
 ORDER BY avg DESC;
